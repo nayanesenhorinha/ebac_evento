55d055627d7a037dc71ae28f1b270a3a66805f32
Nayane de Souza <159593131+nayanesenhorinha@users.noreply.github.com>
2024-10-08 13:48:04 -0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 AOS.init();
 
 const contador = document.getElementById('contador');
-const dataDoEvento = new Date('oct 10, 2023 19:00:00');
+const dataDoEvento = new Date('mar 28, 2025 00:00:00');
 const timeStampDoEvento = dataDoEvento.getTime();
 
 const contaAsHoras = setInterval(() => {
